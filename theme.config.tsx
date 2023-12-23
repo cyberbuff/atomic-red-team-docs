0b65ab8d5354079c5613bbdd127a3506dced634c
Hare Sudhan <code@0x6c.dev>
2023-12-23 04:28:06 -0500

    test og

--- theme.config.tsx
+++ theme.config.tsx
@@ -27,6 +27,8 @@ const config: DocsThemeConfig = {
     const {frontMatter} = useConfig()
     return <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta property="og:image" content="og.png"/>
+      <meta property="og:image:alt" content="AtomicRedTeam"/>
       <meta property="og:title" content={frontMatter.title || "Atomic Red Team docs"}/>
       <meta property="og:description" content={frontMatter.description || "New wiki site for Atomic Red Team"}/>
     </>
