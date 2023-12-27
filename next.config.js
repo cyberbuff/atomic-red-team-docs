e5869dea8560fb718bf38b8f4dca5cfa932d4942
Hare Sudhan <code@0x6c.dev>
2023-12-27 02:02:34 -0500

    test static exports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
+  flexsearch: {
+    codeblocks: false,
+  },
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  }
+})
